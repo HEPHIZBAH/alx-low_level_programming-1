353cc27ddcc014ad231033ce67d4fb1706533a39
Davcid1572 <ifejiofordavid1572@gmail.com>
2022-09-13 08:25:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
- * main - print putchar
- * Return: always 0
+ * main - Print "putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n);
+	char david[] = "_putchar";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(david[i]);
+		i++;
+	}
+	_putchar('\n');
 
 	return (0);
 }
